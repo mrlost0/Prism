571efcf24ccc020a65bebd7e7b1f5948a879b4e1
Lacing <28374234+Lacing@users.noreply.github.com>
2017-12-21 19:19:35 -0600

    [PRISM] - Updated all commands

---
+++ cmds/np.js
@@ -0,0 +1,95 @@
+const botsettings = require("../Storage/botsettings.json");
+const prefixes = require("../Storage/prefixes.json");
+const Discord = require("discord.js");
+const key = process.env.YOUTUBE_API_KEY
+const fs = require("fs"); 
+const colors = require('colors');
+const moment = require('moment');
+const yt = require('ytdl-core');
+const YouTube = require('simple-youtube-api');
+const youtube = new YouTube(key);
+
+exports.run = async(bot, message, args, queue) => {
+  const prefix = prefixes[message.guild.id].prefix
+  const args1 = message.content.split(' ');
+  const searchString = args1.slice(1).join(' ');
+  const url = args1[1] ? args1[1].replace(/<(.+)>/g, '$1') : '';
+  const serverQueue = queue.get(message.guild.id);
+
+    if (!serverQueue) return message.channel.send("There isn't anything playing");
+    return message.channel.send(`🎶 Currently playing: **${serverQueue.songs[0].title}**`);
+ 
+    // Time for the functions
+
+async function handleVideo(video, message, voiceChannel, playlist = false) {
+  const serverQueue = queue.get(message.guild.id);
+  console.log(video);
+  const song = {
+    id: video.id,
+    title: video.title,
+    url: `https://www.youtube.com/watch?v=${video.id}`
+  };
+  if (!serverQueue) {
+    const queueConstruct = { 
+      textChannel: message.channel,
+      voiceChannel: voiceChannel,
+      connection: null,
+      skippers: [],
+      songs: [],
+      volume: 5,
+      playing: true
+    };
+    queue.set(message.guild.id, queueConstruct);
+
+    queueConstruct.songs.push(song);
+
+    try {
+      var connection = await voiceChannel.join();
+      queueConstruct.connection = connection;
+      play(message.guild, queueConstruct.songs[0]);
+    } catch (error) {
+      console.error(`I could not join the voice channel: ${error}`);
+      queue.delete(message.guild.id);
+      return message.channel.send(`I could not join the voice channel: ${error}`);
+    }
+  } else {
+    serverQueue.songs.push(song);
+    console.log(serverQueue.songs);
+    if (playlist) return undefined;
+    else return message.channel.send(`✅ **${song.title}** has been added to the queue!`);
+  }
+  return undefined;
+}
+
+function play(guild, song) {
+  const serverQueue = queue.get(guild.id);
+
+  if (!song) {
+    serverQueue.voiceChannel.leave();
+    queue.delete(guild.id);
+    return;
+  }
+  console.log(serverQueue.songs);
+
+const dispatcher = serverQueue.connection.playStream(yt(song.url))
+        .on('end', reason => {
+            if (reason === 'Stream is not generating quickly enough.') console.log('Song ended.');
+            else console.log(reason);
+            serverQueue.songs.shift();
+            setTimeout(() => {
+                play(guild, serverQueue.songs[0]);
+            }, 250);
+        })
+        .on('error', error => console.error(error));
+    dispatcher.setVolumeLogarithmic(serverQueue.volume / 5);
+
+    serverQueue.textChannel.send(`🎶 Start playing: **${song.title}**`);
+}
+}
+
+exports.help = {
+  name: "np",
+  description: "Shows the music that is playing",
+  usage: "pr!np",
+  note: "Have to be in a voice channel"
+}
